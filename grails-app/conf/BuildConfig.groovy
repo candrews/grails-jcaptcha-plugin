8f5d541f6ddfad078ecb9a2db69c263228e11255
Craig Andrews <candrews@integralblue.com>
2014-07-01 11:30:26 -0400

    Upgrade to Grails 2.4.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,7 +42,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:3.0.1', ':rest-client-builder:2.0.1', {
+		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
 			export = false
 		}
 	}
